24bd13668499210a3caabc2e4188488c19ab039b
imtaka <imtaka1988@gmail.com>
2026-07-29 19:12:20 +0700

    export provider synchronization schema

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,6 +1,9 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
-import * as schema from './schema.js';
+import * as coreSchema from './schema.js';
+import * as providerSyncSchema from './provider-sync-schema.js';
+
+const schema = { ...coreSchema, ...providerSyncSchema };
 
 export function createDatabase(databaseUrl: string) {
   const pool = new Pool({ connectionString: databaseUrl });
@@ -8,4 +11,5 @@ export function createDatabase(databaseUrl: string) {
   return { db, pool };
 }
 
+export * from './provider-sync-schema.js';
 export * from './schema.js';
